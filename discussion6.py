69020a956139f57f279e78be4e436436807ce14d
Nate Mitelman <mitelman@umich.edu>
2026-02-17 15:21:26 -0500

    task #2

--- discussion6.py
+++ discussion6.py
@@ -61,18 +61,20 @@ class HorseRaces:
 ###############################################################################
 
     def horse_fastest_race(self, horse):
-        '''
-        Given the name of a horse, return its fastest race and time.
-        If the horse does not exist, return (None, 999.9)
+        if horse not in self.race_dict:
+            return (None, 999.9)
 
-        Parameters:
-            horse, name of a race: str
+    race_times = self.race_dict[horse] 
 
-        Returns:
-            tuple of fastest race name and the time
-            EXAMPLE: ('Teio Sho', 14.8)
-        '''
-        pass
+    fastest_race = None
+    fastest_time = 999.9
+
+    for race, time in race_times.items():
+        if time < fastest_time:
+            fastest_time = time
+            fastest_race = race
+
+    return (fastest_race, fastest_time)
 
 ###############################################################################
 ##### TASK 3
